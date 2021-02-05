66092cf274f95b6d619bcf9ba7131f1e91249791
Fredy Sarmiento <fasarmiento93@gmail.com>
2021-02-05 11:58:18 -0500

    Inicializacion proyecto

---
+++ src/DesarrolloApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DesarrolloScreen from './components/DesarrolloScreen'
+
+
+const DesarrolloApp = () => {
+    return (
+        <>
+            <DesarrolloScreen />
+        </>
+    )
+}
+export default DesarrolloApp
---
+++ src/components/DesarrolloScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from './Header';
+
+const DesarrolloScreen = () => {
+    return (
+        <div>
+            <main>
+                <Header />
+                <h1>Consideraciones iniciales </h1>
+            </main>
+        </div>
+    )
+}
+export default DesarrolloScreen;

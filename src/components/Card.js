46facdddade0cad30a744cff2e1ba4e4e98584d7
Fredy Sarmiento <fasarmiento93@gmail.com>
2021-02-08 15:31:43 -0500

    encabezado completo

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 export const Card = (props) => {
     return (
-        <>
-            <div className="card_caja">
-                <h2>{props.title_card}</h2>
-                <p> {props.content_card} </p>
-            </div>
-        </>
+        <div className="card">
+                <div className="card_box">
+                    <div className="card_front">
+                        <h2>{props.title_card}</h2>
+                    </div>
+                    <div className="card_back">
+                    <p> {props.content_card} </p>
+                    </div>             
+                </div>
+
+        </div>
     )
 }
